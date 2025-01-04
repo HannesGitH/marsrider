4ab64d64bf7e9ff6d0f5fc6d2e3d060c20b3514f
Hannes <33062605+HannesGitH@users.noreply.github.com>
2025-01-04 22:15:04 +0100

    copied beats

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -2,10 +2,12 @@
   import { XR, Controller, Hand } from "@threlte/xr";
   import { T } from "@threlte/core";
   import Fists from "./Fists.svelte";
+  import Beats from "./Beats.svelte";
 </script>
 
 <XR>
   <Fists></Fists>
+  <Beats></Beats>
   <Controller left/>
 </XR>
 
