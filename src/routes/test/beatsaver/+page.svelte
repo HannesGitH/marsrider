4ef2a1015041ba41f732d639d8c6d0379361f560
Hannes <33062605+HannesGitH@users.noreply.github.com>
2025-01-23 20:16:17 +0100

    ssg

--- src/routes/test/beatsaver/+page.svelte
+++ src/routes/test/beatsaver/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  export const prerender = true
   import type { PageProps } from "./$types";
   import {
     getDefault as getDefaultList,
@@ -37,17 +38,79 @@
       console.log({ off: document.body.offsetHeight, win: window.innerHeight });
     }
   };
+
+  let currSound: HTMLAudioElement | null = null;
+  const playSound = async (url?: string) => {
+    if (!url) return;
+    if (currSound) currSound.pause();
+    currSound = new Audio(url);
+    await currSound.play();
+  };
 </script>
 
 <svelte:window onscroll={onScroll} />
 
+{#snippet figure(image: { src: string; caption: string })}
+  <img src={image.src} alt={image.caption} width="100" height="100" />
+{/snippet}
+
 <ul>
   {#each mapList as mapData}
-    <li>
-      {mapData.name}
+    <li class="mapListItem">
+      <span>
+        {mapData.name}
+      </span>
+      {#if mapData.versions}
+        {#each mapData.versions as v}
+          <div
+            class="mapListDetails"
+            role="button"
+            onmouseenter={() => playSound(v.previewURL)}
+            tabindex={0}
+          >
+            {@render figure({
+              src: v.coverURL!,
+              caption: `${mapData.name} cover`,
+            })}
+            {#if v.diffs}
+              <ul>
+                {#each v.diffs as diff}
+                  <li>
+                    <span>{diff.difficulty}</span>
+                  </li>
+                {/each}
+              </ul>
+            {/if}
+            <spacer></spacer>
+            <span>{v.downloadURL}</span>
+          </div>
+        {/each}
+      {/if}
     </li>
   {/each}
 </ul>
 {#if isLoading}
   <div class="loadingIndicator">Loading...</div>
 {/if}
+
+<style>
+  .mapListItem {
+    padding: 1rem;
+    border-bottom: 1px solid #ccc;
+    list-style-type: none;
+    .mapListDetails {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+    }
+  }
+
+  spacer {
+    flex-grow: 1;
+  }
+
+  .loadingIndicator {
+    padding: 1rem;
+    text-align: center;
+  }
+</style>
